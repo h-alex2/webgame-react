6a205600f9b76996736884b31218c8d8a09d294c
h-alex2 <glowhyun1@gmail.com>
2022-06-19 23:21:42 +0900

    🎉 build: add structure

---
+++ 3.숫자야구/client.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NumberBaseball from "./NumberBaseball-hooks";
+import StyledComponents from "./StyledComponents"
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <NumberBaseball />
+)
