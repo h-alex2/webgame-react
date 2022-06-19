946ef28382fafc72e8cfc59293fe773bdae2803b
h-alex2 <glowhyun1@gmail.com>
2022-06-19 23:14:55 +0900

    🎉 build: add structure

---
+++ 2.끝말잇기/client.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import WordRelayClass from "./WordRelay-class"
+
+ReactDom.createRoot(document.querySelector("#root")).render(
+
+  <WordRelayClass />
+)
