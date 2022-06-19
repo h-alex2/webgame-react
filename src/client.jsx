5781ac5404d63befcacecb835eabdb2eb53c7c71
h-alex2 <glowhyun1@gmail.com>
2022-06-20 00:11:59 +0900

    🎨 perf: add 숫자야구

--- src/client.jsx
+++ src/client.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom"
 import Gugudan from "./Gugudan";
 import WordRelayHooks from "./WordRelay-hooks";
+import NumberBaseball from "./NumberBaseball-hooks";
 
 const Collect = () => {
 	return (
 		<>
-		<div>
-			<h2>구구단</h2>
-			<Gugudan />
-		</div>
-		<br />
-		<hr />
-		<div>
-			<h2>끝말잇기</h2>
-			<WordRelayHooks />
-		</div>
+      <div>
+        <h2>구구단</h2>
+        <Gugudan />
+      </div>
+      <br />
+      <hr />
+      <div>
+        <h2>끝말잇기</h2>
+        <WordRelayHooks />
+      </div>
+      <br />
+      <hr />
+      <div>
+        <h2>숫자야구</h2>
+        <NumberBaseball />
+      </div>
 		</>
 	)
 }
