b2cdc30420515edb99f7b4ac555a162fbe5115ad
h-alex2 <glowhyun1@gmail.com>
2022-06-20 13:24:11 +0900

    🎉 build: 반응속도체크 project

---
+++ 4.반응속도체크/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ResponseCheck from "./ResponseCheck";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <ResponseCheck />
+)
