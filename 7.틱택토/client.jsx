1139494a4ca465cf929392ff82574bb1d22b5075
h-alex2 <glowhyun1@gmail.com>
2022-06-23 11:04:17 +0900

    🎉 build: 틱택토 project

---
+++ 7.틱택토/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TicTacToe from "./TicTacToe";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <TicTacToe />
+)
